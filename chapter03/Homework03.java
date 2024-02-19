374d368bf4dda847c828a693d897d91ca482cfef
王宗滢 <96827706+WangZongying1@users.noreply.github.com>
2024-02-19 17:07:01 +0800

    上传文件夹

---
+++ chapter03/Homework03.java
@@ -0,0 +1,21 @@
+// + 在不同情况下的区别
+
+
+public class Homework03 {
+	public static void main(String[] args) {
+		String book1 = "天龙八部";
+		String book2 = "笑傲江湖";
+		System.out.println(book1 + book2);//天龙八部笑傲江湖
+
+		char c1 = '男';
+		char c2 = '女';
+		System.out.println(c1 + c2);//得到字符码值的和
+
+		//保存两本书的价格
+		double price1 = 123.56;
+		double price2 = 100.11;
+		System.out.println(price1+price2);
+	}
+
+
+}
